eeb97f42a5a3c478969390b46c2a9ff48e961de0
JosefAM <jafmalheiro@live.com.pt>
2020-03-03 22:49:48 +0000

    Função validaCoord

--- modules/coordenada.c
+++ modules/coordenada.c
@@ -9,3 +9,11 @@ void adicionaCoordenada (char *fileName, char *coord, int player)
 {
     atualizarTab("temp", 3,4);
 }
+
+int validaCoord (char s[3]){
+    int resp;
+    resp = 0;
+    if ( s[0] >= 'a' && s[0] <= 'h' && s[1] > '0' && s[1] <= '8') resp = 1;
+    return resp; //Caso a resp (Resposta) for 1 - Coordenada Válida/ Caso contrário - 0
+}
+
--- modules/coordenada.h
+++ modules/coordenada.h
@@ -4,5 +4,9 @@
 
 #ifndef RASTROS_COORDENADA_H
 #define RASTROS_COORDENADA_H
+
 void adicionaCoordenada (char *fileName, char *coord, int player);
+
+int validaCoord (char s[3]);
+
 #endif //RASTROS_COORDENADA_H
