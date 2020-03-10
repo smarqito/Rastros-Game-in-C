8f9b8f9346deb74e6d52601a4a04fa64218ad512
JosefAM <jafmalheiro@live.com.pt>
2020-03-10 00:06:32 +0000

    Funções jogar - isPossiblePlay (atualizado)

--- main.c
+++ main.c
@@ -1,24 +1,11 @@
 #include "modules/data.h"
 #include "modules/interpretador.h"
+#include "modules/logica/logica.h"
 
 int main() {
     ESTADO *e = initState();
     interpretador(e);
+    COORDENADA cord = {1,1};
+    jogar(e,cord);
     return 0;
 }
-/*
-int main() {
-    int i;
-    char ip[MAXCHAR],c;
-    printf("Bem vindo ao jogo Rastros\n");
-
-    while (!sair(ip)){
-        printf(("Diga-nos a sua instrução: "));
-        for(i=0; (c=getchar()) != '\n' && i < MAXCHAR; i++)
-            ip[i] = c;
-        ip[i]='\0';
-        executaInstr(ip);
-    }
-
-    return 0;
-}*/
--- modules/logica/logica.c
+++ modules/logica/logica.c
@@ -6,42 +6,27 @@
 #include "logica.h"
 #include "../data.h"
 
-int isPossiblePlay1 (ESTADO *state, COORDENADA c){
-    JOGADA ultima = state->jogadas[];
-    int linhaU1 = ultima.jogador1.linha, colunaU1 = ultima.jogador1.coluna;
+int isPossiblePlay (ESTADO *state, COORDENADA c){
+    COORDENADA ultima = state->ultimaJogada;
+    int linhaU = ultima.linha, colunaU = ultima.coluna;
     int linhaA = c.linha, colunaA = c.coluna;
     int resposta = 0;
-    if( linhaA == linhaU1 + 1 || linhaA == linhaU1 - 1 || linhaA == linhaU1){
-        if (colunaA == colunaU1 + 1 || colunaA == colunaU1 - 1 || colunaA == colunaU1){
+    if( linhaA == linhaU + 1 || linhaA == linhaU - 1 || linhaA == linhaU){
+        if (colunaA == colunaU + 1 || colunaA == colunaU - 1 || colunaA == colunaU){
             resposta = 1;
         }
     }
-    if (linhaA == linhaU1 && colunaA == colunaU1) resposta = 0;
+    if (linhaA == linhaU && colunaA == colunaU) resposta = 0;
     return resposta;
 }
-
-int isPossiblePlay2 (ESTADO *state, COORDENADA c){
-    JOGADA ultima = state->jogadas[];
-    int linhaU2 = ultima.jogador2.linha, colunaU2 = ultima.jogador2.coluna;
-    int linhaA = c.linha, colunaA = c.coluna;
-    int resposta = 0;
-    if( linhaA == linhaU2 + 1 || linhaA == linhaU2 - 1 || linhaA == linhaU2){
-        if (colunaA == colunaU2 + 1 || colunaA == colunaU2 - 1 || colunaA == colunaU2){
-            resposta = 1;
-        }
-    }
-    if (linhaA == linhaU2 && colunaA == colunaU2) resposta = 0;
-    return resposta;
-}
-
-int isPossiblePlay (ESTADO *estado, COORDENADA c){
-    int resposta;
-    if()
-}
-
 
 int jogar (ESTADO *state, COORDENADA c){
-    printf("jogar %d %d\n", c.coluna, c.linha);
-    if (getHouseState(*state,c) == VAZIA && isPossiblePlay(*state, c)){
-
+    int jogadaP = 0;
+    if (getHouseState(state,c) == VAZIO && isPossiblePlay(state, c)){
+        state-> tab[c.linha][c.coluna] = BRANCA;
+        jogadaP = 1;
+        printf("jogar %d %d\n", c.coluna, c.linha);
+    }
+    else printf("Não é possível efetuar essa jogada! Tente Novamente.");
+    return jogadaP;
 }
