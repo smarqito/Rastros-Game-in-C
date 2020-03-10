8ccd43e9c4a7facb2cffb3db349fb4d34a3f3011
Marco Antonio Sousa <smarqito@gmail.com>
2020-03-10 01:01:22 +0000

    interface melhorado

--- modules/interface/interface.c
+++ modules/interface/interface.c
@@ -23,12 +23,31 @@ char converteCasa (CASA house) {
 
 void mostrarTabuleiro (ESTADO *state) {
     int m,n;
+    char i;
+    for(n=0; n<2;n++)
+        putchar(' ');
+    putchar(' ');
+    for(i='a';i<'i';i++)
+        printf("___ ");
+    printf("\n");
     for (m=0; m<MAX_HOUSES;m++) {
+        printf("%d | ",m+1);
         for(n=0;n<MAX_HOUSES;n++) {
-            if (!m && n == 7) printf("2");
-            else if (m==7 && !n) printf("1 ");
-            else printf("%c ", converteCasa(state->tab[m][n]));
+            if (!m && n == 7) printf("2 | ");
+            else if (m==7 && !n) printf("1 | ");
+            else printf("%c | ", converteCasa(state->tab[m][n]));
         }
         printf("\n");
+        for(n=0; n<2;n++)
+            putchar(' ');
+        putchar('|');
+        for(i='a';i<'i';i++)
+            printf("___|");
+        printf("\n");
     }
+    for(n=0; n<4;n++)
+        putchar(' ');
+    for(i='a';i<'i';i++)
+        printf("%c   ",i);
+    putchar('\n');
 }
