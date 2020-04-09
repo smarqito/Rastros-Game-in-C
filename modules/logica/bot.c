74d9507488000ebc1b5de4294f8002918a3fa0be
Marco António Sousa <smarqito@gmail.com>
2020-04-09 23:54:49 +0100

    bot com heurística aleatória

--- modules/logica/bot.c
+++ modules/logica/bot.c
@@ -2,14 +2,31 @@
 // Created by marco on 3/10/20.
 //
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "../listas/listas.h"
 #include "logica.h"
 #include "bot.h"
+#include "../interface/interface.h"
 
 int jogaBot (ESTADO *state) {
+    int r,i;
+    srand(time(0));
+
+    LISTA l = coordenadasPossiveis(state);
+    COORDENADA *jogadaBot;
+    r = lengthLista(l);
     
-    printf("pede ajuda ao bot.");
+    for(i = rand() % r; i>0; i--){
+        proximo(l);
+
+    }
+    
+    jogadaBot=devolveCabeca(l);
+
+    jogar(state,*jogadaBot);
+
     return 0;
 }
 
