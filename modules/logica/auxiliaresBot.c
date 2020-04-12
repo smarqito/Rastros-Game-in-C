7636695966ee34e4e2e9635b7ed1290e401ae32c
JosefAM <jafmalheiro@live.com.pt>
2020-04-12 01:18:38 +0100

    heuristica distancia euclidiana

--- modules/logica/auxiliaresBot.c
+++ modules/logica/auxiliaresBot.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #include "auxiliaresBot.h"
 #include "../data.h"
@@ -52,6 +53,48 @@ int botRandom (LISTA coordPossiveis, COORDENADA **jogada) {
 
     return r;
 }
+
+
+int calculaDist (COORDENADA *a, COORDENADA b){
+    int linhaA = a->linha, colunaA = a->coluna;
+    int linhaB = b.linha, colunaB = b.coluna;
+    int distancia;
+
+    distancia = sqrt(((linhaA - linhaB)^2) + ((colunaA - colunaB)^2));
+
+    return distancia;
+}
+
+
+int distanciaJog (ESTADO *state, LISTA coordPossiveis, COORDENADA **jogada) {
+    int distancia, distanciaMenor = 50;
+    int tamanho = lengthLista(coordPossiveis);
+    LISTA r;
+    COORDENADA *resposta, Jogador1, Jogador2;
+    Jogador1.linha = 1;
+    Jogador1.coluna = 1;
+    Jogador2.linha = 8;
+    Jogador2.coluna = 8;
+    if (!tamanho) {
+        if (obterJogador(state) == 1) {
+            for (r = coordPossiveis; r && r != NULL; r = r->proxima) {
+                distancia = calculaDist(r->valor, Jogador1);
+                if (distancia < distanciaMenor) resposta = r->valor;
+            }
+        }
+        if (obterJogador(state) == 2) {
+            for (r = coordPossiveis; r && r != NULL; r = r->proxima) {
+                distancia = calculaDist(r->valor, Jogador2);
+                if (distancia < distanciaMenor) resposta = r->valor;
+            }
+
+        }
+        (*jogada) = resposta;
+    }
+
+    return tamanho;
+}
+
 
 int jogaBot (ESTADO *state) {
     int r;
